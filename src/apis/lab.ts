38a332758e70d25f754702a2c2ce5d756a2c81d2
hye1ee <hyewon0809@kaist.ac.kr>
2023-08-24 22:08:50 +0900

    [chore] Fix build error

--- src/apis/lab.ts
+++ src/apis/lab.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios"
 const { VITE_LAB_HOST } = import.meta.env;
 
 export const testApi = async () => {
-  return axios.post(VITE_LAB_HOST + '/create_rubric').then((res) => {
+  return axios.post(VITE_LAB_HOST + '/create_rubric').then((res: any) => {
     console.log(res);
   })
 }
