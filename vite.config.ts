c365f7597523fd15a12a7024074362d7ef90573f
hye1ee <hyewon0809@kaist.ac.kr>
2023-08-25 18:58:05 +0900

    [chore] Modify for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { ssr } from 'vite-plugin-ssr/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'http://ec2-43-201-59-27.ap-northeast-2.compute.amazonaws.com',
-  plugins: [react(), tsconfigPaths(), ssr({
-    baseAssets: 'http://ec2-43-201-59-27.ap-northeast-2.compute.amazonaws.com',
-    // baseServer: 'http://ec2-43-201-59-27.ap-northeast-2.compute.amazonaws.com'
-  })],
+  plugins: [react(), tsconfigPaths()],
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
   server: {
+    proxy: {
+      '/': {
+        target: 'http://ec2-43-201-59-27.ap-northeast-2.compute.amazonaws.com/'
+      }
+    }
   },
 })
