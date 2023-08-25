cbfc18e072d85690463c5ca5bd083aab99530dbd
hye1ee <hyewon0809@kaist.ac.kr>
2023-08-25 18:49:15 +0900

    [chore] Modify for build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'http://http://ec2-43-201-59-27.ap-northeast-2.compute.amazonaws.com',
+  base: 'http://ec2-43-201-59-27.ap-northeast-2.compute.amazonaws.com',
   plugins: [react(), tsconfigPaths()],
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
