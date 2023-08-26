b72e8aebd872c9647c41add9b40a2a179a79fce9
hye1ee <hyewon0809@kaist.ac.kr>
2023-08-26 21:26:43 +0900

    [chore] Fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,10 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'http://ec2-43-201-59-27.ap-northeast-2.compute.amazonaws.com/',
   plugins: [react(), tsconfigPaths()],
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
   server: {
-    https: false,
   },
 })
