754bf998b3f00223c1f98d5610634dc4fffa3f7a
hye1ee <hyewon0809@kaist.ac.kr>
2023-08-31 14:28:47 +0900

    [feat] Implement alert

--- src/apis/lab.ts
+++ src/apis/lab.ts
@@ -8,27 +8,26 @@ export const getRubric = async (script: string) => {
   return axios.post(VITE_LAB_HOST + '/create_rubric', { selected_script: script }).then((res: any) => {
     console.log(res.data);
     return res.data;
-  })
+  }).catch((() => alert('Network Error! Please try again.')))
 }
 
 export const getDialogue = async (data: any): Promise<any[]> => {
 
   return axios.post(VITE_LAB_HOST + '/dialogue_generation', data).then((res: any) => {
     return res.data;
-  })
+  }).catch((() => alert('Network Error! Please try again.')))
 }
 
 export const getDialogueBase = async (data: any): Promise<any[]> => {
 
   return axios.post(VITE_LAB_HOST + '/dialogue_generation_baseline', data).then((res: any) => {
     return res.data;
-  })
-
+  }).catch((() => alert('Network Error! Please try again.')))
 }
 
 export const getVariation = async (data: any): Promise<string[]> => {
 
   return axios.post(VITE_LAB_HOST + '/generate_variations', data).then((res: any) => {
     return res.data;
-  })
+  }).catch((() => alert('Network Error! Please try again.')))
 }
